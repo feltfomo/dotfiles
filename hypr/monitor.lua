a43c6d0ea0b60e49b332ef6455ed08602be1b0eb
feltfomo <241195017+feltfomo@users.noreply.github.com>
2026-05-11 18:13:45 -0700

    fixed monitor.lua and made it detect what machine its on

--- hypr/monitor.lua
+++ hypr/monitor.lua
@@ -1,6 +1,23 @@
-hl.monitor({
-    output   = "eDP-1",
-    mode     = "1920x1080@60",
-    position = "0x0",
-    scale    = "1",
-})
+local hostname = io.popen("hostname"):read("*l")
+
+if hostname == "lumi" then
+	hl.monitor({
+		output = "eDP-1",
+		mode = "1920x1080@60",
+		position = "0x0",
+		scale = "1",
+	})
+elseif hostname == "khion" then
+	hl.monitor({
+		output = "DP-1",
+		mode = "2560x1440@180",
+		position = "0x0",
+		scale = "1",
+	})
+	hl.monitor({
+		output = "DP-2",
+		mode = "2560x1440@180",
+		position = "2560x0",
+		scale = "1",
+	})
+end
